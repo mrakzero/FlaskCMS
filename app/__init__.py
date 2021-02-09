a40aed2384a2ad216623d6d5e60366f7c16d6c4b
gallanthunter <gallanthunter@163.com>
2021-02-09 20:03:29 +0800

    初始化文件

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from config import config
+
+# 由于尚未初始化所需的程序实例，所以没有初始化扩展，创建扩展类时没有向构造函数传入参数。
+
+mail = Mail()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    '''工厂函数'''
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)  # 通过config.py统一接口
+    mail.init_app(app)  # 同上
+    db.init_app(app)  # 同上
+
+    # 附加路由和自定义错误页面，将蓝本注册到工厂函数
+
+
+    return app
+
+
