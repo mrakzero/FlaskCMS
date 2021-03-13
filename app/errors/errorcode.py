f6f8be812bfea3589555e34d173f77d70c80ff2c
gallanthunter <gallanthunter@163.com>
2021-03-13 15:10:25 +0800

    update category and errorcode file.

---
+++ app/errors/errorcode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# version: 1.0.0
+# author:Zhang Zhijun
+# time: 2021-03-13
+# file: errorcode.py
+# function:
+# modify:
+
+class ErrorCode:
+    code = ''
+    message = ''
+
+    def __init__(self,code,message):
+        self.code = code
+        self.message = message
+
+
+
--- app/views/admin/category.py
+++ app/views/admin/category.py
@@ -6,10 +6,29 @@
 # file: category.py
 # function:
 # modify:
-from flask import Blueprint
+from flask import Blueprint, render_template, jsonify
 
-bp_admin_category = Blueprint('admin_category',__name__, url_prefix='/admin', template_folder='../template',static_folder='../static')
+from app.forms.category import CategoryForm
+from app.models.category import Category
 
-@bp_admin_category.route('/category',methods=['GET'])
+bp_admin_category = Blueprint('admin_category', __name__, url_prefix='/admin', template_folder='../template',
+                              static_folder='../static')
+
+
+@bp_admin_category.route('/category', methods=['GET'])
 def category():
     category_form = CategoryForm()
+    categories = Category.query.all()
+    return render_template('admin/category/category.html', category_form=category_form, categories=categories)
+
+@bp_admin_category.route('/category/list', methods=['GET'])
+def get_all_category():
+    try:
+        categories = Category.query.filter().all()
+    except:
+        return jsonify(code=12,message='query filed.')
+    if categories == None:
+        return jsonify(code=12,message='result is none.')
+    category_form = CategoryForm()
+    categories = Category.query.all()
+    return render_template('admin/category/category.html', category_form=category_form, categories=categories)
