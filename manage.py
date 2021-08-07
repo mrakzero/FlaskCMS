3b760d7e8a5c9c32d55b14a478842df49da7d22f
gallanthunter <gallanthunter@163.com>
2021-08-07 21:56:26 +0800

    1. upgrade Flask fron verion 1.1.11 to 2.0.1 2. remove Flask-script, use flask db init/migrade/upgrade instead of manage.py db init/migrate/upgrade 3. upgrade requirements.txt

--- manage.py
+++ manage.py
@@ -3,8 +3,7 @@
 import os
 
 from app import create_app, db
-from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 
 from app.models.category import Category
 from app.models.comment import Comment
@@ -15,7 +14,6 @@ from app.models.tag import Tag
 from app.models.user import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
 migrate = Migrate(app, db)
 
 
@@ -24,16 +22,5 @@ def make_shell_context():
                 Site=Site)
 
 
-manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
-
-
-@manager.command
-def test():
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-
 if __name__ == '__main__':
-    manager.run()
+    app.run()
