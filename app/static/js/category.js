e896233c85c60b02adf510f11ef89145dd73cd15
gallanthunter <gallanthunter@163.com>
2021-09-20 17:30:34 +0800

    1、更新前台文件

---
+++ app/static/js/category.js
@@ -0,0 +1,42 @@
+
+layui.use('table', function () {
+    var table = layui.table;
+
+    table.render({
+        elem: '#categories-table'
+        , url: '/admin/categories'
+        , title: '文章分类列表'
+        , cols: [[
+
+            {field: 'id', title: 'ID', width: 80, fixed: 'left', unresize: true, sort: true}
+            , {field: 'name', title: '分类名', width: 120, edit: 'text'}
+            , {field: 'slug', title: '分类别名', width: 80, edit: 'text', sort: true}
+            , {field: 'description', title: '分类描述', width: 100}
+            , {fixed: 'right', title: '操作', toolbar: '#barDemo', width: 150}
+        ]]
+        , page: true
+    });
+
+    //监听行工具事件
+    table.on('tool(categories-table)', function (obj) {
+        var data = obj.data;
+        //console.log(obj)
+        if (obj.event === 'del') {
+            layer.confirm('真的删除行么', function (index) {
+                obj.del();
+                layer.close(index);
+            });
+        } else if (obj.event === 'edit') {
+            layer.prompt({
+                formType: 2
+                , value: data.email
+            }, function (value, index) {
+                obj.update({
+                    email: value
+                });
+                layer.close(index);
+            });
+        }
+    });
+});
+
